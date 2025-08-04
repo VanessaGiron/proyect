a849ff3d0f8d35ca118363aa877a33a9c3ecd3b0
VanessaGiron11 <hubgit079@gmail.com>
2025-08-03 23:57:54 -0600

    Rol y pom

--- src/main/java/com/esfe/proyect/Modelos/Roles.java
+++ src/main/java/com/esfe/proyect/Modelos/Roles.java
@@ -1,5 +1,28 @@
 package com.esfe.proyect.Modelos;
 
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+
+@Entity
 public class Roles {
+     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer Id;
+
+    @NotBlank(message = "El nombre de rol es requerido")
+    private String Nombre;
+
+    public Integer getId(){
+        return Id;
+    }
+    public void setId(Integer Id){
+        this.Id = Id;
+    }
+    public String getNombre(){
+        return Nombre;
+    }
+    public void setNombre(String Nombre){
+        this.Nombre = Nombre;
+    }
 
 }
