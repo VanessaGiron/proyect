4560362b1b15d63bf6e1bd66766a33ac6a0e9191
palaguti <davidpalacios0987@gmail.com>
2025-08-03 22:25:18 -0600

    Implementacion de entidad clientes y dependencias

---
+++ src/main/java/com/esfe/proyect/Modelos/Clientes.java
@@ -0,0 +1,68 @@
+package com.esfe.proyect.Modelos;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.*;
+
+@Entity
+public class Clientes {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @NotBlank(message = "El nombre es requerido")
+    private String nombre;
+
+    @NotBlank(message = "El apellido es requerido")
+    private String apellido;
+
+    @NotBlank(message = "La dirección es requerida")
+    private String direccion;
+
+    @NotBlank(message = "El teléfono es requerido")
+    private String telefono;
+
+    public Clientes() {}
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+}
+
