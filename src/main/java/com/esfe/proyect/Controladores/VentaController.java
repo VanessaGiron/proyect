ea282f5b632c8d81033492e51488c13d3a8a7f22
cp25001@esfe.agape.edu.sv <cp25001@esfe.agape.edu.sv>
2025-08-26 19:44:45 -0600

    cambio

--- src/main/java/com/esfe/proyect/Controladores/VentaController.java
+++ src/main/java/com/esfe/proyect/Controladores/VentaController.java
@@ -22,7 +22,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
-import com.esfe.proyect.Modelos.Cliente;
+
 import com.esfe.proyect.Modelos.Venta;
 import com.esfe.proyect.Servicios.interfaces.IClienteService;
 import com.esfe.proyect.Servicios.interfaces.IVentaService;
